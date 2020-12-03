70c36b7bfea2ed93d32f4978acdc419312e07e16
huyongming <1005697035@qq.com>
2020-12-03 14:48:17 +0800

    动态规划：leetcode：474. 一和零（动态规划版本）

---
+++ src/com/hym/datastructure/algorithm/dynamicplan/MaxFormDynamic.java
@@ -0,0 +1,78 @@
+package com.hym.datastructure.algorithm.dynamicplan;
+
+public class MaxFormDynamic {
+
+    public static int findMaxForm(String[] strs, int m, int n) {
+        if (strs == null || strs.length == 0) {
+            return 0;
+        }
+        int size = strs.length;
+        int[] oneSums = new int[size];
+        int[] zeroSums = new int[size];
+        for (int i = 0; i < size; i++) {
+            String str = strs[i];
+            if (str != null) {
+                int oneSum = 0;
+                int zeroSum = 0;
+                for (int j = 0; j < str.length(); j++) {
+                    if (str.charAt(j) == '1') {
+                        oneSum++;
+                    }
+                    if (str.charAt(j) == '0') {
+                        zeroSum++;
+                    }
+                }
+                oneSums[i] = oneSum;
+                zeroSums[i] = zeroSum;
+            }
+        }
+        //记录状态值
+        int[][][] flags = new int[size][m + 1][n + 1];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    flags[i][j][k] = -1;
+                }
+            }
+        }
+        //初始化：每个阶段在其他阶段都不选的情况下可达的状态和对应的值
+        for (int i = 0; i < size; i++) {
+            flags[i][0][0] = 0;
+            if (zeroSums[i] <= m && oneSums[i] <= n) {
+                flags[i][zeroSums[i]][oneSums[i]] = 1;
+            }
+        }
+        /**
+         * 状态转移公式：
+         * flags[i][j][k]=max(flags[i - 1][j][k],flags[i][j][k]);
+         * flags[i][j + zeroSums[i]][k + oneSums[i]]=max(flags[i][j + zeroSums[i]][k + oneSums[i]],flags[i - 1][j][k] + 1)
+         */
+        for (int i = 1; i < size; i++) {
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    if (flags[i - 1][j][k] >= 0) {
+                        if (flags[i - 1][j][k] > flags[i][j][k]) {
+                            flags[i][j][k] = flags[i - 1][j][k];
+                        }
+                        if (j + zeroSums[i] <= m && k + oneSums[i] <= n) {
+                            if (flags[i - 1][j][k] + 1 > flags[i][j + zeroSums[i]][k + oneSums[i]]) {
+                                flags[i][j + zeroSums[i]][k + oneSums[i]] = flags[i - 1][j][k] + 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        //遍历最后一个阶段，找出最大值
+        int max = -1;
+        for (int j = 0; j <= m; j++) {
+            for (int k = 0; k <= n; k++) {
+                if (flags[size - 1][j][k] > max) {
+                    max = flags[size - 1][j][k];
+                }
+            }
+        }
+
+        return max;
+    }
+}
---
+++ src/com/hym/datastructure/algorithm/dynamicplan/MaxFormDynamicTest.java
@@ -0,0 +1,41 @@
+package com.hym.datastructure.algorithm.dynamicplan;
+
+import org.junit.Test;
+
+public class MaxFormDynamicTest {
+
+    @Test
+    public void test() {
+//        strs = ["10", "0001", "111001", "1", "0"], m = 5, n = 3
+        String[] strings = {"10", "0001", "111001", "1", "0"};
+        int m = 5;
+        int n = 3;
+        int max = MaxFormDynamic.findMaxForm(strings, m, n);
+        System.out.println("max:" + max);
+    }
+
+    @Test
+    public void test2() {
+        //strs = ["10", "0", "1"], m = 1, n = 1
+        String[] strings = {"10", "0", "1"};
+        int m = 1;
+        int n = 1;
+        int max = MaxFormDynamic.findMaxForm(strings, m, n);
+        System.out.println("max:" + max);
+    }
+
+    @Test
+    public void test3() {
+        //strs = ["10", "0", "1"], m = 1, n = 1
+//        ["011","1","11","0","010","1","10","1","1","0","0","0","01111","011","11","00","11","10","1","0","0","0","0","101","001110","1","0","1","0","0","10","00100","0","10","1","1","1","011","11","11","10","10","0000","01","1","10","0"]
+//        44
+//        39
+        String[] strings = {"011", "1", "11", "0", "010", "1", "10", "1", "1", "0", "0", "0", "01111", "011", "11", "00", "11", "10", "1", "0", "0", "0", "0", "101", "001110", "1", "0", "1", "0", "0", "10", "00100", "0", "10", "1", "1", "1", "011", "11", "11", "10", "10", "0000", "01", "1", "10", "0"};
+        int m = 44;
+        int n = 39;
+        int max = MaxFormDynamic.findMaxForm(strings, m, n);
+        System.out.println("max:" + max);
+    }
+
+
+}
