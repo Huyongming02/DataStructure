460a333f31bfb0fb8a100331888e0c1812ff1b8a
huyongming <1005697035@qq.com>
2020-12-03 15:20:33 +0800

    动态规划：leetcode：474. 一和零（动态规划-使用二维数组记录状态值）

--- src/com/hym/datastructure/algorithm/dynamicplan/MaxFormDynamic2.java
+++ src/com/hym/datastructure/algorithm/dynamicplan/MaxFormDynamic2.java
@@ -9,23 +9,7 @@ public class MaxFormDynamic2 {
         int size = strs.length;
         int[] oneSums = new int[size];
         int[] zeroSums = new int[size];
-        for (int i = 0; i < size; i++) {
-            String str = strs[i];
-            if (str != null) {
-                int oneSum = 0;
-                int zeroSum = 0;
-                for (int j = 0; j < str.length(); j++) {
-                    if (str.charAt(j) == '1') {
-                        oneSum++;
-                    }
-                    if (str.charAt(j) == '0') {
-                        zeroSum++;
-                    }
-                }
-                oneSums[i] = oneSum;
-                zeroSums[i] = zeroSum;
-            }
-        }
+        countZeroAndOneNum(strs, size, oneSums, zeroSums);
         //记录状态值
         int[][] flags = new int[m + 1][n + 1];
         for (int j = 0; j <= m; j++) {
@@ -33,21 +17,17 @@ public class MaxFormDynamic2 {
                 flags[j][k] = -1;
             }
         }
-        //初始化：每个阶段在其他阶段都不选的情况下可达的状态和对应的值
-        for (int i = 0; i < size; i++) {
-            flags[0][0] = 0;
-            if (zeroSums[i] <= m && oneSums[i] <= n) {
-                flags[zeroSums[i]][oneSums[i]] = 1;
-            }
+        //第一阶段
+        flags[0][0] = 0;
+        if (zeroSums[0] <= m && oneSums[0] <= n) {
+            flags[zeroSums[0]][oneSums[0]] = 1;
         }
         /**
-         * 状态转移公式：
-         * flags[i][j][k]=max(flags[i - 1][j][k],flags[i][j][k]);
-         * flags[i][j + zeroSums[i]][k + oneSums[i]]=max(flags[i][j + zeroSums[i]][k + oneSums[i]],flags[i - 1][j][k] + 1)
+         * 状态转移公式：flags[j + zeroSums[i]][k + oneSums[i]] =max(flags[j][k] + 1, flags[j + zeroSums[i]][k + oneSums[i]])
          */
         for (int i = 1; i < size; i++) {
-            for (int j = 0; j <= m; j++) {
-                for (int k = 0; k <= n; k++) {
+            for (int j = m; j >= 0; j--) {
+                for (int k = n; k >= 0; k--) {
                     if (flags[j][k] >= 0) {
                         if (j + zeroSums[i] <= m && k + oneSums[i] <= n) {
                             if (flags[j][k] + 1 > flags[j + zeroSums[i]][k + oneSums[i]]) {
@@ -69,5 +49,25 @@ public class MaxFormDynamic2 {
         }
 
         return max;
+    }
+
+    private static void countZeroAndOneNum(String[] strs, int size, int[] oneSums, int[] zeroSums) {
+        for (int i = 0; i < size; i++) {
+            String str = strs[i];
+            if (str != null) {
+                int oneSum = 0;
+                int zeroSum = 0;
+                for (int j = 0; j < str.length(); j++) {
+                    if (str.charAt(j) == '1') {
+                        oneSum++;
+                    }
+                    if (str.charAt(j) == '0') {
+                        zeroSum++;
+                    }
+                }
+                oneSums[i] = oneSum;
+                zeroSums[i] = zeroSum;
+            }
+        }
     }
 }
